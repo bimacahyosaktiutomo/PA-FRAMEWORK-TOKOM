18434383a5d5e968d12e7e9f75e7cee40fd07859
NatanaelPLHN <natannaelprimuslung@gmail.com>
2024-11-21 10:03:47 +0800

    crud minus updatenya

--- tokom/tokom/urls.py
+++ tokom/tokom/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('product_details/', views.product_details, name='product_details'),
     path('cart/', views.cart, name='cart'),
+    
+    
+    path('add_item/', views.add_item, name='add_item'), #ADD ITEM DI DASHBOARD
+    path('delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
--- tokom/tokom/views.py
+++ tokom/tokom/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Item, Category, Stock
+from .forms import ItemForm
 
 def homepage(request):
     return render(request, 'homepage/index.html')
@@ -11,6 +13,39 @@ def product_details(request):
 def cart(request):
     return render(request, 'pages/cart.html')
 
+# @login_required
+# def dashboard(request):
+#     return render(request, 'dashboard/dashboard.html')
+
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    items = Item.objects.select_related('category').all()
+    return render(request, 'dashboard/dashboard.html', {'items': items})
+
+# ADD ITEM DI DASHBOARD
+def add_item(request):
+    # Check if categories exist; if not, create a default category
+    if not Category.objects.exists():
+        default_category = Category.objects.create(name="--------")
+
+    items = Item.objects.all()
+
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tokom:dashboard')  # Redirect after successful form submission
+    else:
+        form = ItemForm()
+
+    categories = Category.objects.all()  # Fetch categories again if a new one was created
+    return render(request, 'dashboard/add_item.html', {'form': form, 'items': items, 'categories': categories})
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, item_id=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('tokom:dashboard')
+        
+    return render(request, 'dashboard/delete_item.html', {'item': item})
+

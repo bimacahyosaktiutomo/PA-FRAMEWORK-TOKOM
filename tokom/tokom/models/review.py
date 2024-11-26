d09787ee7389ce2e52e6eea8b2495bd282c7f248
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2024-11-27 01:20:45 +0800

    fix (product_details) : ngebalikin gambar + nambah field gambar di review

--- tokom/tokom/models/review.py
+++ tokom/tokom/models/review.py
@@ -6,6 +6,7 @@ class Review(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='reviews')
     review_text = models.CharField(max_length=50)
     rating = models.FloatField(null=True, blank=True)
+    image = models.ImageField(upload_to='reviews/', null=True, blank=True)
 
     def __str__(self):
         return f"Review for {self.item.name}"
